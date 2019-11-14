8f742480ec224bb34925734418980546b7eb81ce
BanupriyaSelvaraj <56014875+BanupriyaSelvaraj@users.noreply.github.com>
2019-11-14 18:09:47 +0530

    Added files

---
+++ bin/src/app.component.ts
@@ -0,0 +1,9 @@
+import {Component, ViewEncapsulation} from '@angular/core';
+
+@Component({
+  selector: 'ej-app',
+  templateUrl: 'src/app.component.html',
+  styleUrls:['src/app.component.css']
+})
+export class AppComponent {
+}
